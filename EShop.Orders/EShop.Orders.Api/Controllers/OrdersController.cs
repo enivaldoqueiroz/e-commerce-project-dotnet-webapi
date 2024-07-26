3b9d7f6d84263f985566b2e8eae8e8890a4c1553
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-07-26 10:17:08 -0300

    Implementação do controller OrderController e os endpoints Get e Post

---
+++ EShop.Orders/EShop.Orders.Api/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using EShop.Orders.Application.Commands;
+using EShop.Orders.Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Orders.Api.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id) 
+        {
+            var query = new GetOrderById(id);
+
+            var result = await _mediator.Send(query);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(AddOrder command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Get), new { id }, command);
+        }
+    }
+}
